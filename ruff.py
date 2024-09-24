f794dc2c994f28d17557b9e38128a2fd7c7c53bf
tkkkd <>
2024-09-24 22:45:52 +0900

    restore python codes

--- ruff.py
+++ ruff.py
@@ -1,7 +1,10 @@
 from typing import Iterable
 
 import os
+<<<<<<< HEAD
 
+=======
+>>>>>>> 39d713e (restore python codes)
 
 def sum_even_numbers(numbers: Iterable[int]) -> int:
     """Given an iterable of integers, return the sum of all even numbers in the iterable."""
